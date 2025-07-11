62fc2968e1caa5526b92b7bec4e183d7328d02a0
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-11 17:37:48 +0500

    Add src\components\Footer.jsx

---
+++ src\components\Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="bg-gray-200 py-4">
+      <div className="container mx-auto p-4">
+        <p className="text-gray-600">Copyright 2023 Tourism Website</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
